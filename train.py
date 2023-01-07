dace6aff602855134af54251056977c9d6a99bfe
lenovo-ubuntu22.04 <lenovo-ubuntu22.04>
2023-01-07 14:09:24 +0800

    update

--- train.py
+++ train.py
@@ -8,140 +8,166 @@ import shutil
 from module import *
 from emailtool import *
 
-# 使用GPU加速进行训练
-# 定义训练的设备
-dev = torch.device(device="cuda" if torch.cuda.is_available() else "cpu")
 
-# 训练完成是否发送邮件通知
-email_flag = True
-email_addr = "Atm991014@163.com"
+def train(epoch=200, dev="cuda", email=True, email_addr="Atm991014@163.com", accuracy_level=0.95, tensorboard=True):
 
-# 训练的轮数
-epoch = 200
+    # epoch:迭代次数
+    # dev:训练设备
+    # email:训练完成是否发送邮件通知
+    # email_addr:接收通知的邮箱地址
+    # accuracy_level:当训练集准确率大于accuracy_level,会进行测试。测试集准确率大于accuracy_level会进行模型保存并结束训练
+    # tensorboard:是否使用tensorboard绘制训练曲线
 
-# 当训练集大于此数值会进行测试
-accuracy_level = 0.95
+    # 定义训练的设备
+    dev = torch.device(device=dev if torch.cuda.is_available() else "cpu")
 
-# 数据集准备
-train_data = torchvision.datasets.CIFAR10(
-    root="./dataset",
-    train=True,
-    transform=torchvision.transforms.ToTensor(),
-    download=True,
-)
-test_data = torchvision.datasets.CIFAR10(
-    root="./dataset",
-    train=True,
-    transform=torchvision.transforms.ToTensor(),
-    download=True,
-)
-
-# 数据集加载
-train_loader = DataLoader(dataset=train_data, batch_size=64)
-test_loader = DataLoader(dataset=test_data, batch_size=64)
-
-# 创建网络模型,转移至训练设备
-module = Mymodule().to(device=dev)
-
-# 损失函数,转移至训练设备
-loss_fn = nn.CrossEntropyLoss().to(device=dev)
-
-# 优化器
-learning_rate = 1e-2
-optimizer = torch.optim.SGD(params=module.parameters(), lr=learning_rate)
-
-# 设置训练网络的一些参数
-# 记录训练的次数
-
-train_step = 0
-
-# 记录测试的次数
-test_step = 0
-
-# 使用tensorboard画出训练曲线
-if os.path.exists("train_logs"):
-    shutil.rmtree("train_logs")
-writer = SummaryWriter("train_logs")
-
-# 设置模型存储目录
-save_path = "model_file"
-save_path = os.path.join(save_path, str(time.time()))
-os.makedirs(save_path)
-
-# -----开始训练-----
-start_time = time.time()
-for i in range(epoch):
-    print("-----第{}轮训练开始-----".format(i + 1))
-    total_train_accuracy = 0  # 记录每次迭代的训练集准确率
-    total_train_loss = 0  # 记录每次迭代的总误差值
-    # 训练步骤
-    # module.train() # 设定为训练模式，仅对某些特殊层生效，具体看说明文档
-    for data in train_loader:
-        imgs, labels = data
-        # 转移至训练设备
-        imgs = imgs.to(dev)
-        labels = labels.to(dev)
-        outputs = module(imgs)
-
-        # 统计正确预测的数量
-        total_train_accuracy += (outputs.argmax(1) == labels).sum()
-        loss = loss_fn(outputs, labels)
-        total_train_loss += loss
-        # 优化器优化模型
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        train_step += 1
-    # 计算本轮训练集的正确率
-    total_train_accuracy = total_train_accuracy / len(train_data)
-    print("-----训练次数:{}-----,loss:{}".format(train_step, loss))
-    print("-----训练集正确率:{}-----".format(total_train_accuracy))
-    print(f"-----用时:{time.time()-start_time:.2f}秒-----")
-
-    # 绘制训练曲线图
-    writer.add_scalar(tag="train_loss", scalar_value=total_train_loss, global_step=i+1)
-    writer.add_scalar(tag="train_accuracy", scalar_value=total_train_accuracy, global_step=i+1)
-
-    # 训练集准确度达标则进行测试
-    if total_train_accuracy >= accuracy_level:
-        # 测试步骤开始
-        total_test_accuracy = 0  # 记录每次迭代的测试集正确率
-        total_test_loss = 0
-        # module.eval() # 设定为验证模式，仅对某些特殊层生效，具体看说明文档
-        with torch.no_grad():  # 去掉梯度，保证测试过程不会对网络模型的参数调优
-            for data in test_loader:
-                imgs, labels = data
-                # 转移至训练设备
-                imgs = imgs.to(dev)
-                labels = labels.to(dev)
-
-                outputs = module(imgs)
-                total_test_accuracy += (outputs.argmax(1) == labels).sum()
-                loss = loss_fn(outputs, labels)
-                total_test_loss += loss
-        total_test_accuracy = total_test_accuracy / len(test_data)
-        print("-----测试集Loss:{}-----".format(total_test_loss))
-        print("-----测试集正确率:{}-----".format(total_test_accuracy))
-        test_step += 1
-        # 测试集准确度达标则进行保存，并且退出迭代训练
-        if total_test_accuracy >= accuracy_level:
-            torch.save(
-                module, "{}/module_epoch={}_accuracy=".format(save_path, i, total_test_accuracy))
-            break
-# -----迭代结束-----
-
-# 如果没有模型保存，则进行模型保存
-if not os.listdir(save_path):
-    torch.save(module, "{}/module_epoch={}_accuracy=".format(save_path,
-               epoch, total_test_accuracy))
-writer.close()
-end_time = time.time()
-elapsed_time = end_time - start_time
-print(f"Elapsed time: {elapsed_time:.2f} seconds")
-sendemail = Email("Atm991014@163.com")
-if email_flag:
-    sendemail.send(
-        "训练完成<br/>测试集Loss:{}<br/>测试集正确率:{}<br/>迭代次数:{}<br/>训练总次数:{}<br/>用时:{}秒".format(
-            total_test_loss, total_test_accuracy, i, train_step, elapsed_time
-        )
+    # 数据集准备
+    train_data = torchvision.datasets.CIFAR10(
+        root="./dataset",
+        train=True,
+        transform=torchvision.transforms.ToTensor(),
+        download=True,
     )
+    test_data = torchvision.datasets.CIFAR10(
+        root="./dataset",
+        train=True,
+        transform=torchvision.transforms.ToTensor(),
+        download=True,
+    )
+
+    # 数据集加载
+    train_loader = DataLoader(dataset=train_data, batch_size=64)
+    test_loader = DataLoader(dataset=test_data, batch_size=64)
+
+    # 创建网络模型,转移至训练设备
+    module = Mymodule().to(device=dev)
+
+    # 损失函数,转移至训练设备
+    loss_fn = nn.CrossEntropyLoss().to(device=dev)
+
+    # 优化器
+    learning_rate = 1e-2
+    optimizer = torch.optim.SGD(params=module.parameters(), lr=learning_rate)
+
+    # 设置训练网络的一些参数
+    # 记录训练的次数
+
+    train_step = 0
+
+    # 记录测试的次数
+    test_step = 0
+
+    if tensorboard:
+        # 使用tensorboard画出训练曲线
+        if os.path.exists("train_logs"):
+            shutil.rmtree("train_logs")
+        writer = SummaryWriter("train_logs")
+
+    # 设置模型存储目录
+    save_path = "model_file"
+    save_path = os.path.join(save_path, str(time.time()))
+    os.makedirs(save_path)
+
+    # -----开始训练-----
+    print("-----开始训练-----")
+    start_time = time.time()
+    for i in range(1, epoch+1):
+        print("-----第 {} 轮训练开始-----".format(i))
+        total_train_accuracy = 0  # 记录每次迭代的训练集准确率
+        total_train_loss = 0  # 记录每次迭代的总误差值
+        total_test_accuracy = 0  # 记录每次迭代的测试集正确率
+        total_test_loss = 0  # 记录每次迭代的总误差值
+        # 训练步骤
+        # module.train() # 设定为训练模式，仅对某些特殊层生效，具体看说明文档
+        for data in train_loader:
+            imgs, labels = data
+            # 转移至训练设备
+            imgs = imgs.to(dev)
+            labels = labels.to(dev)
+
+            # 将数据输入模型
+            outputs = module(imgs)
+
+            # 统计训练集正确预测的数量
+            total_train_accuracy += (outputs.argmax(1) == labels).sum()
+
+            # 累加训练集的损失值
+            loss = loss_fn(outputs, labels)
+            total_train_loss += loss
+
+            # 优化器优化模型
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_step += 1
+        # 计算本轮训练集的正确率
+        total_train_accuracy = total_train_accuracy / len(train_data)
+        print("-----训练次数: {} -----,loss:{}".format(train_step, loss))
+        print("-----训练集正确率: {} -----".format(total_train_accuracy))
+        print(f"-----总用时: {time.time()-start_time:.2f} 秒-----")
+
+        # 绘制训练曲线图
+        if tensorboard:
+            writer.add_scalar(tag="train_loss", scalar_value=total_train_loss, global_step=i)
+            writer.add_scalar(tag="train_accuracy", scalar_value=total_train_accuracy, global_step=i)
+
+        # 训练集准确度达标则进行测试
+        if total_train_accuracy >= accuracy_level or i == epoch:
+            # 测试步骤开始
+            # module.eval() # 设定为验证模式，仅对某些特殊层生效，具体看说明文档
+            with torch.no_grad():  # 去掉梯度，保证测试过程不会对网络模型的参数调优
+                for data in test_loader:
+                    imgs, labels = data
+                    # 转移至训练设备
+                    imgs = imgs.to(dev)
+                    labels = labels.to(dev)
+                    # 将数据输入模型
+                    outputs = module(imgs)
+
+                    # 统计测试集正确预测的数量
+                    total_test_accuracy += (outputs.argmax(1) == labels).sum()
+
+                    # 累加测试集的损失值
+                    loss = loss_fn(outputs, labels)
+                    total_test_loss += loss
+
+            # 计算测试集正确率
+            total_test_accuracy = total_test_accuracy / len(test_data)
+            print("-----测试集Loss: {} -----".format(total_test_loss))
+            print("-----测试集正确率: {} -----".format(total_test_accuracy))
+            test_step += 1
+            # 测试集准确度达标则进行保存，并且退出迭代训练
+            if total_test_accuracy >= accuracy_level:
+                savemodule(MODULE=module, PATH=save_path, EPOCH=i, ACCURACY=total_test_accuracy)
+                break
+    # -----迭代结束-----
+    print("-----训练完成-----")
+
+    # 如果没有模型保存，则进行模型保存
+    if not os.listdir(save_path):
+        savemodule(MODULE=module, PATH=save_path, EPOCH=i, ACCURACY=total_test_accuracy)
+    writer.close()
+
+    # 计算训练用时并输出
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"-----训练总用时: {elapsed_time:.2f} 秒")
+
+    # 发送邮件
+    if email:
+        print("-----发送邮件-----")
+        sendemail = Email(email_addr)
+        sendemail.send(
+            "训练完成<br/>测试集Loss:{}<br/>测试集正确率:{}<br/>迭代次数:{}<br/>训练总次数:{}<br/>用时:{}秒".format(
+                total_test_loss, total_test_accuracy, i, train_step, elapsed_time
+            )
+        )
+
+
+def savemodule(MODULE, PATH, EPOCH, ACCURACY):
+    print("-----保存模型参数-----")
+    torch.save(MODULE, "{}/module_epoch={}_accuracy={}".format(PATH, EPOCH, ACCURACY))
+
+
+if __name__ == "__main__":
+    train(epoch=1, email=True)
